8791eb79723f1185dbd2be4f14fb9a1b517926a5
maqvn <maqvn@cin.ufpe.br>
2026-05-11 14:19:09 -0300

    Divisão em app e server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,5 @@
-import express from 'express';
-import cors from 'cors';
 import 'dotenv/config';
-
-import authRoutes from './routes/auth.routes.js';
-import courseRoutes from './routes/courses.routes.js';
-import lessonsRoutes from './routes/lessons.routes.js';
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/auth', authRoutes);
-app.use('/api/courses', courseRoutes);
-app.use('/api/lessons', lessonsRoutes);
+import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
